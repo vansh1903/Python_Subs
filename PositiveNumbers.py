ebb7cfcc089a296a57f9ce5fe85cd36f6a410d95
vansh1903 <80567728+vansh1903@users.noreply.github.com>
2021-05-11 12:01:23 +0530

    Update PositiveNumbers.py

--- PositiveNumbers.py
+++ PositiveNumbers.py
@@ -1,3 +1,8 @@
-n = int(input("Enter range:"))
-for i in range(n)
-  print(n)
+x = []
+n = int(input("Enter limit:"))
+for i in range(n):
+    v = int(input("Enter any number:"))
+    x.append(v)
+for j in x:
+    if j > 0:
+        print(j)
